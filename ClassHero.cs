7d27f5632b2fa3e6e09ef6bcf116da00d4fafe97
Your Name <you@example.com>
2024-02-28 03:45:14 +0300

    test

---
+++ ClassHero.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OOP_z1
+{
+    public class Specifications
+    { 
+        private double hp; // здоровье
+        private double damage; // урон
+        private int armor; // броня
+        private double evasionChance; // процент уворота
+
+        public static readonly int MinDamage = 1; // минимальное значение урона
+        public static readonly int MaxDamage = 20; // максимальное значение урона
+
+        // Конструктор по умолчанию
+        public Specifications()
+        {
+            hp = 0; // здоровье по умолчанию
+            damage = 0; // урон по умолчанию
+            armor = 0; // броня по умолчанию
+            evasionChance = 0; // процент уворота по умолчанию
+        }
+
+        // Конструктор класса с начальным здоровьем, уроном, броней и процентом уворота
+        public Specifications(double startHp, double startDamage, int startArmor, double startEvasionChance)
+        {
+            hp = startHp; // начальное здоровье 
+            damage = startDamage; // начальный урон
+            armor = startArmor; // начальная броня
+            evasionChance = startEvasionChance; // начальный процент уворота
+        }
+
+        // Получение текущего здоровья
+        public double GetHp()
+        {
+            return hp;
+        }
+
+        // Присвоение значений здоровья
+        public void SetHp(double newHp)
+        {
+            const int min = 50; // минимальное значение здоровья
+            const int max = 100; // максимальное значение здоровья
+            if (newHp >= min && newHp <= max)
+            {
+                hp = newHp;
+            }
+            else
+            {
+                throw new ArgumentException("Некорректное значение здоровья!");
+            }
+        }
+
+        // Получение текущего урона
+        public double GetDamage()
+        {
+            return damage;
+        }
+
+        // Присвоение значений урона
+        public void SetDamage(double newDamage)
+        {
+            if (newDamage >= MinDamage && newDamage <= MaxDamage)
+            {
+                damage = newDamage;
+            }
+            else
+            {
+                throw new ArgumentException("Некорректное значение урона!");
+            }
+        }
+
+        // Получение текущей брони
+        public int GetArmor()
+        {
+            return armor;
+        }
+
+        // Присвоение значений брони
+        public void SetArmor(int newArmor)
+        {
+            const int min = 0; // минимальное значение брони
+            const int max = 20; // максимальное значение брони
+            if (newArmor >= min && newArmor <= max)
+            {
+                armor = newArmor;
+            }
+            else
+            {
+                throw new ArgumentException("Некорректное значение брони!");
+            }
+        }
+
+        // Получение текущего процента уворота
+        public double GetEvasionChance()
+        {
+            return evasionChance;
+        }
+
+        // Присвоение значений процента уворота
+        public void SetEvasionChance(double newEvasionChance)
+        {
+            const double min = 0; // минимальное значение процента уворота
+            const double max = 0.8; // максимальное значение процента уворота
+            if (newEvasionChance >= min && newEvasionChance <= max)
+            {
+                evasionChance = newEvasionChance;
+            }
+            else
+            {
+                throw new ArgumentException("Некорректное значение шанса уворота!");
+            }
+        }
+
+        // Получение урона с учётом блока
+        public void TakeDamage(double receivedDamage)
+        {
+            double evasionRoll = new Random().NextDouble(); // генерация от 0 до 1   
+            if (evasionRoll > evasionChance) // проверка на уворот
+            {
+                hp -= receivedDamage; // уменьшение здоровья
+            }
+            else
+            {
+                MessageBox.Show("Игрок: увернулся.");
+            }
+        }
+
+        // Атака
+        public void Attack(Specifications bot)
+        {
+            double block = 1 - (bot.GetArmor() * 0.05); // блок урона
+            double inflictedDamage = damage * block; // урон после блока
+            double evasionRoll = new Random().NextDouble(); // генерация от 0 до 1
+                                                            // проверка на уворот
+            if (evasionRoll > bot.GetEvasionChance())
+            {
+                bot.TakeDamage(inflictedDamage); // вызов получения урона
+            }
+            else
+            {
+                MessageBox.Show("Враг: увернулся.");
+            }
+        }
+
+       
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace OOP_z1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //
+        private Specifications character;
+        private Specifications enemy;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Createbutton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                character = new Specifications(); // объекта класса Specifications
+                characteristic(character); // заполнение характеристик
+                enemy = new Specifications(100,4,5,0.5);
+                // вывод информации о созданном объекте
+                MessageBox.Show($"Создан персонаж:\nЗдоровье: {character.GetHp()}\nУрон: {character.GetDamage()}\nБроня: {character.GetArmor()}\nШанс уворота: {character.GetEvasionChance()}");
+                MessageBox.Show($"Создан персонаж:\nЗдоровье: {enemy.GetHp()}\nУрон: {enemy.GetDamage()}\nБроня: {enemy.GetArmor()}\nШанс уворота: {enemy.GetEvasionChance()}");
+                
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Randomcreatebutton_Click(object sender, RoutedEventArgs e)
+        {
+            while (character.GetHp() > 0 && enemy.GetHp() > 0)
+            {
+                character.Attack(enemy); //атака игрока
+                MessageBox.Show("HP врага: " + enemy.GetHp()); //здоровье врага после атаки
+                if (enemy.GetHp() <= 0)
+                {
+                    break;
+                }
+                enemy.Attack(character); //атака врага
+                MessageBox.Show("HP игрока: " + character.GetHp()); //здоровье игрока после атаки
+                if (character.GetHp() <= 0)
+                {
+                    break;
+                }
+                MessageBox.Show("\n");
+            }
+
+            MessageBox.Show("\n");
+
+            if (character.GetHp() <= 0)
+            {
+                MessageBox.Show("Игрок проиграл");
+            }
+            else if (enemy.GetHp() <= 0)
+            {
+                MessageBox.Show("Враг проиграл");
+            }
+        }
+        /// заполнение характеристик
+        void characteristic(Specifications hero)
+        {
+            double hp = double.Parse(healthTextBox.Text);
+            double dmg = double.Parse(damageTextBox.Text);
+            int armor = int.Parse(armorTextBox.Text);
+            double eva = double.Parse(evasionTextBox.Text);
+            hero.SetHp(hp); //присвоения здоровья
+            hero.SetDamage(dmg); //присвоения урона;
+            hero.SetArmor(armor); //присвоения брони
+            hero.SetEvasionChance(eva / 100.0); //присвоения шанса уворота
+        }
+
+
+
+        private void Box_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Onlynumbers(e.Text); //ввод только чисел
+        }
+
+        private bool Onlynumbers(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); // только цифры
+            return !regex.IsMatch(text);
+        }
+
+        ///Тесты
+        void test()
+        {
+            Specifications a; //объект для теста 1
+            Specifications b;
+            a = new Specifications();
+            b = new Specifications(70,10,13,0); //объект для теста 2
+            a.SetHp(55); //присвоить объекту a, 55 здоровья
+            a.SetDamage(5);//присвоить объекту a, 5 урона
+            a.SetArmor(5);//присвоить объекту a, 5 брони
+            a.SetEvasionChance(0);//присвоить объекту a, 0 процента уворота  
+            Debug.Assert(55 == a.GetHp()); //проверка присвоения здоровья объекту а
+            Debug.Assert(5 == a.GetDamage());//проверка присвоения урона объекту а
+            Debug.Assert(5 == a.GetArmor());//проверка присвоения брони объекту а
+            Debug.Assert(0 == a.GetEvasionChance());//проверка присвоения процента уворота объекту а
+            a.Attack(b); //атака объекта a по объекту b
+            b.Attack(a); //атака объекта b по объекту а
+            Debug.Assert(47.5 == a.GetHp());//проверка уменьшения здоровья объекта а
+            Debug.Assert(68.25 == b.GetHp());//проверка уменьшения здоровья объекта b
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            test();
+        }
+    }
+}
